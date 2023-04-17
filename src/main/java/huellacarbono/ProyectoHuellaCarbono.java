b21815d2ecd1f1431f1f8044863adfdb2fe04d97
Victoria-Sampalo <victoriasampalo19@gmail.com>
2023-04-17 14:09:07 +0200

    primer commit, creación del proyecto

---
+++ src/main/java/huellacarbono/ProyectoHuellaCarbono.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package huellacarbono;
+
+/**
+ *
+ * @author victoria
+ */
+public class ProyectoHuellaCarbono {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
