ef564b401cfe9a1511821396156894463610fb93
edacalm990 <edacalm990@g.educaand.es>
2023-04-17 14:31:16 +0200

    creacion de paquete http

--- src/main/java/huellacarbono/MainProyectoHuellaCarbono.java
+++ src/main/java/huellacarbono/MainProyectoHuellaCarbono.java
@@ -9,7 +9,7 @@ package huellacarbono;
  *
  * @author victoria
  */
-public class ProyectoHuellaCarbono {
+public class MainProyectoHuellaCarbono {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
