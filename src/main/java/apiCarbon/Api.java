302cf764e6f60bfede5ab34d43d3987bf063a434
edacalm990 <edacalm990@g.educaand.es>
2023-04-17 14:00:08 +0100

    creando servicio json

--- src/main/java/apiCarbon/Api.java
+++ src/main/java/apiCarbon/Api.java
@@ -2,7 +2,6 @@
 package apiCarbon;
 
 import java.io.Serializable;
-import javax.annotation.Generated;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
